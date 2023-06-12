b3b891fcd4aad6cc3f5ecd2899d137ec1878b7dd
Ciencat <pinchu_uonlrasanye@163.com>
2023-06-12 23:02:35 +0800

    add ex1.py'

---
+++ ex1.py
@@ -0,0 +1,9 @@
+#print("hello world",end="")
+#print("Hello Agaiin!",end="")
+#print("i like this",end="")
+print("This is fun",end="")
+print("Yay! printing!",end="")
+print("I'd like much rather you not",end="")
+print("I said 'do not touch this'",end="")
+print("hello pythom~~",end="")
+
