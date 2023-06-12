28bc6af5dbd46d65ee81e1696ec2307c3d420aff
Ciencat <pinchu_uonlrasanye@163.com>
2023-06-12 23:54:13 +0800

    add 2 exercise'

---
+++ code/ex2.py
@@ -0,0 +1,7 @@
+# A comment ,this is so you can read your program later.
+# Anthing after the # is ignore b python.
+
+print("I could have code like this.") # DAAAAAAAA
+#DSAFJLK aklagjg    
+#klaaaaaaaal
+print("this will run.")
